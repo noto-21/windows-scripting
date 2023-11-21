8662d5393ba00b545e3ff1e90345e4c2cf3d94dd
NT <128177967+noto-21@users.noreply.github.com>
2023-11-21 12:37:03 -0500

    Powershell Scripts

---
+++ PowerShell/P1.ps1
@@ -0,0 +1,5 @@
+﻿Get-Process | 
+Where-Object { $_.WS -gt 100MB } | 
+Sort-Object WS | 
+Select-Object Name, @{Name='MemoryUsageMB'; Expression={($_.WorkingSet / 1MB)}} | 
+ConvertTo-Html -Property Name, MemoryUsageMB > MemoryIntensiveProcesses.html
---
+++ PowerShell/P2.ps1
@@ -0,0 +1,5 @@
+﻿Get-Service | 
+Where-Object { $_.Status -eq "Stopped" } | 
+Sort-Object DisplayName | 
+Select-Object DisplayName, Status, ServiceName | 
+ConvertTo-Csv -NoTypeInformation > StoppedServices.csv
